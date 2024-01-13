0020470a199bb066531bd1a73bb152ec3625b4ac
Shine0078 <128758030+Shine0078@users.noreply.github.com>
2024-01-12 23:47:16 -0500

    Updated all files
    * Added file introduction as comments
    * completed all scripted instructions and requirements

--- script/list.js
+++ script/list.js
@@ -1,4 +1,13 @@
-// ADDING ITEMS TO START AND END OF LIST
+/*
+	Name: Samuel Abraham
+	Course : Web Development - CSS
+	Student id: 100870571
+	Program : Computer Programming 
+	Descriptions: this file contain the list.js website
+	Date: 2024-01-12
+*/
+
+// ADDING ITEMS TO START AND END OF LIST                        #END OF LIST
 // Get the <ul> element
 var groceryList = document.getElementById("groceries");
 
@@ -12,29 +21,38 @@ newListItem.appendChild(listItemContent);
 // Add element end of list
 groceryList.appendChild(newListItem);
 
-// ADD NEW ITEM START OF LIST
+
+// ADD NEW ITEM START OF LIST                                     #START OF LIST
 // Create element
+const NewListItems = document.createElement("Tomato");
 // Create text node
+const ListItemsContent = document
 // Add text node to element
+NewListItems.appendChild(ListItemsContent);
 // Add element to list
+groceryList.appendChild(NewListItems);
 
 
 
 // All <li> elements
-var listItems = groceryList.getElementsByClassName("li");
+var listItems = groceryList.getElementsByClassName("apple");
 // ADD A CLASS OF COOL TO ALL LIST ITEMS
 // Counter variable
 // Loop through elements
 for (let i = 0; i < listItems.length; i++) {    
-    // Change class to cool
-    listItems.item(i).classList.add("hueframe");
+// Change class to cool
+    listItems.item(i).classList.add("cool");
   
 }
 
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
 // h2 element
+var heading = document.querySelector("h2");
 // h2 text
 // No. of <li> elements
+var itemCount = groceryList.getElementsByTagName("li").length;
 // Content
+var headingContent = "Buy groceries -->Total Items: " + itemCount ;
 // Update h2 using innerHTML (not textContent) because it contains markup
+heading.innerHTML = headingContent;
