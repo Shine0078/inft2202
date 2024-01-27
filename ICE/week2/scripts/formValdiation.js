deea8112d6f0e1413a501bd44539d3ae637e94f5
Shine0078 <128758030+Shine0078@users.noreply.github.com>
2024-01-27 06:43:42 -0500

    CHANGES

--- ICE/week2/scripts/formValdiation.js
+++ ICE/week2/scripts/formValdiation.js
@@ -27,18 +27,20 @@ function validateEmailAddressSimple(emailString) {
 * @returns {boolean}    validation result
 */
 function validateEmailAddressRegex(emailString) {
-
+    //the regular expression to validate the email address for string+string|number bewten 2-20 characters
+    // note the / and / at the beginning and end of the expression
     var emailRegex = /^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$/;
+
     //return true if the email address is valid - how to use regex
 	return !!emailString && typeof emailString === 'string'
 		&& emailString.match(emailRegex);
 }
-
 /*
  * Validate the username using a regular expression
  * @param {string} username   ------> the username to validate
  * @returns {boolean}         ------> if the username is valid
  */
+
 function validateUsername(username) {
  
     var usernameRegex = /^[a-z0-9_-]{3,15}$/;
@@ -48,54 +50,18 @@ function validateUsername(username) {
 }
 
 
-
-/** 
- * Validate the form when submitted
- * @returns {boolean} - Indicates if the form is valid
- */
-function validateForm() {
-    // Clear any previous errors and highlighting
-    clearErrors();
-
-    // Validate email
-    const emailInput = document.getElementById("email");
-    if (!validateEmailAddressRegex(emailInput.value)) {
-        updateErrorMessage("Invalid email address.");
-        highlightError();
-        return false;
-    }
-
-    // Validate username
-    const usernameInput = document.getElementById("username");
-    if (!validateUsername(usernameInput.value)) {
-        updateErrorMessage("Invalid username.");
-        highlightError();
-        return false;
-    }
-
-    return true;
-}
+//TODO:
+// Make all fields required (HTML)
+// Add a pattern attribute in the telephone element (HTML)
+// Ensure the password is redacted into dots on screen when typed in (HTML)
+// Create a function that knows if the username is valid (feel free to grab some RegEx from the Interwebs - cite source and test it!)
+// If it is not valid, be sure to update the #generalError <p></p> with a good error message and some highlighting (red background, maybe?)
+// Validate the username when the form is submitted
+// Clear any additional error message and highlighting when the form is reset
+// COMMENT EVERYTHING :D
 
 
-function clearErrors() {
-    const generalError = document.getElementById("generalError");
-    generalError.innerText = "This Text is INVALID , Please Try Again";
-    generalError.classList.remove("highlight-error");
-}
 
-/**
- * Update the #generalError <p></p> with an error message
- * @param {string} message - The error message to display
- */
-function updateErrorMessage(message) {
-    const generalError = document.getElementById("generalError");
-    generalError.innerText = message;
-}
 
-/**
- * Highlight the #generalError with a red background
- */
-function highlightError() {
-    const generalError = document.getElementById("generalError");
-    generalError.classList.add("highlight-error");
-}
+
+
